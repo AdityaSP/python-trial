6972df2ae18b8a9b55aa66e1718e40a7dc04a579
Aditya <sp.aditya@gmail.com>
2018-05-30 21:57:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ import unittest
 class TestMy(unittest.TestCase):
   def test_add(self):
     assert 1==1
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
