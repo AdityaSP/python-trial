0784f4ff75241255e3ff26c91202b462afa7aa1a
Aditya <sp.aditya@gmail.com>
2018-05-30 19:23:51 +0530

    Create loganalysis.py

---
+++ loganalysis.py
@@ -0,0 +1,16 @@
+import re
+p = r'^\[.{31}\] \[(.*?)\]'
+cp = re.compile(p)
+
+d = {}
+
+
+with open('error.log') as fh:
+    for line in fh:
+        #m = re.search(p, line)
+        m = cp.search(line)
+        if m:
+            #d[':error'] = d[':error'] + 1
+            d[m.group(1)] = d.get(m.group(1),0) + 1
+
+print d
